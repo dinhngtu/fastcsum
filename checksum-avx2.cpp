9d116a94f0244bdad07d654445f174817752bdaf
Tu Dinh <1257909+dinhngtu@users.noreply.github.com>
2024-06-17 18:19:41 +0200

    avx2: refactor

--- checksum-avx2.cpp
+++ checksum-avx2.cpp
@@ -57,76 +57,73 @@ static uint64_t csum_31bytes(const uint8_t *b, size_t size, uint64_t initial) {
     return ac;
 }
 
+static inline void addc_epi32(__m256i &s, __m256i &c, __m256i a, __m256i b, __m256i mask) {
+    s = _mm256_add_epi32(a, b);
+    c = _mm256_cmpgt_epi32(_mm256_xor_si256(mask, a), _mm256_xor_si256(mask, s));
+    c = _mm256_srli_epi32(c, 31);
+}
+
+static inline uint64_t addc_fold_epi32(__m256i &v, uint64_t initial) {
+    unsigned long long ac = initial;
+    uint64_t out[4];
+    unsigned char c;
+    _mm256_storeu_si256(reinterpret_cast<__m256i_u *>(&out[0]), v);
+    c = _addcarry_u64(0, ac, static_cast<uint64_t>(out[0]), &ac);
+    c = _addcarry_u64(c, ac, static_cast<uint64_t>(out[1]), &ac);
+    c = _addcarry_u64(c, ac, static_cast<uint64_t>(out[2]), &ac);
+    c = _addcarry_u64(c, ac, static_cast<uint64_t>(out[3]), &ac);
+    ac += c;
+    return ac;
+}
+
 uint64_t fastcsum_nofold_avx2(const uint8_t *b, size_t size, uint64_t initial) {
     unsigned long long ac = initial;
-    unsigned char carry;
-    __m256i vac{};
-    __m256i vdt, vsum, vcarry;
+    __m256i vac = _mm256_setzero_si256();
+    __m256i v, s, c;
     __m256i mask = _mm256_set1_epi32(0x80000000);
 
     while (size >= 128) {
-        vdt = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b));
-        vsum = _mm256_add_epi32(vac, vdt);
-        vcarry = _mm256_cmpgt_epi32(_mm256_xor_si256(mask, vac), _mm256_xor_si256(mask, vsum));
-        vcarry = _mm256_srli_epi32(vcarry, 31);
-        vac = _mm256_add_epi32(vsum, vcarry);
+        v = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b));
+        addc_epi32(s, c, vac, v, mask);
+        vac = _mm256_add_epi32(s, c);
 
-        vdt = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b + 32));
-        vsum = _mm256_add_epi32(vac, vdt);
-        vcarry = _mm256_cmpgt_epi32(_mm256_xor_si256(mask, vac), _mm256_xor_si256(mask, vsum));
-        vcarry = _mm256_srli_epi32(vcarry, 31);
-        vac = _mm256_add_epi32(vsum, vcarry);
+        v = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b + 32));
+        addc_epi32(s, c, vac, v, mask);
+        vac = _mm256_add_epi32(s, c);
 
-        vdt = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b + 64));
-        vsum = _mm256_add_epi32(vac, vdt);
-        vcarry = _mm256_cmpgt_epi32(_mm256_xor_si256(mask, vac), _mm256_xor_si256(mask, vsum));
-        vcarry = _mm256_srli_epi32(vcarry, 31);
-        vac = _mm256_add_epi32(vsum, vcarry);
+        v = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b + 64));
+        addc_epi32(s, c, vac, v, mask);
+        vac = _mm256_add_epi32(s, c);
 
-        vdt = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b + 96));
-        vsum = _mm256_add_epi32(vac, vdt);
-        vcarry = _mm256_cmpgt_epi32(_mm256_xor_si256(mask, vac), _mm256_xor_si256(mask, vsum));
-        vcarry = _mm256_srli_epi32(vcarry, 31);
-        vac = _mm256_add_epi32(vsum, vcarry);
+        v = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b + 96));
+        addc_epi32(s, c, vac, v, mask);
+        vac = _mm256_add_epi32(s, c);
 
         b += 128;
         size -= 128;
     }
     if (size >= 64) {
-        vdt = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b));
-        vsum = _mm256_add_epi32(vac, vdt);
-        vcarry = _mm256_cmpgt_epi32(_mm256_xor_si256(mask, vac), _mm256_xor_si256(mask, vsum));
-        vcarry = _mm256_srli_epi32(vcarry, 31);
-        vac = _mm256_add_epi32(vsum, vcarry);
+        v = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b));
+        addc_epi32(s, c, vac, v, mask);
+        vac = _mm256_add_epi32(s, c);
 
-        vdt = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b + 32));
-        vsum = _mm256_add_epi32(vac, vdt);
-        vcarry = _mm256_cmpgt_epi32(_mm256_xor_si256(mask, vac), _mm256_xor_si256(mask, vsum));
-        vcarry = _mm256_srli_epi32(vcarry, 31);
-        vac = _mm256_add_epi32(vsum, vcarry);
+        v = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b + 32));
+        addc_epi32(s, c, vac, v, mask);
+        vac = _mm256_add_epi32(s, c);
 
         b += 64;
         size -= 64;
     }
     if (size >= 32) {
-        vdt = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b));
-        vsum = _mm256_add_epi32(vac, vdt);
-        vcarry = _mm256_cmpgt_epi32(_mm256_xor_si256(mask, vac), _mm256_xor_si256(mask, vsum));
-        vcarry = _mm256_srli_epi32(vcarry, 31);
-        vac = _mm256_add_epi32(vsum, vcarry);
+        v = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b));
+        addc_epi32(s, c, vac, v, mask);
+        vac = _mm256_add_epi32(s, c);
 
         b += 32;
         size -= 32;
     }
 
-    __v4di out[4];
-    _mm256_store_si256(&out[0], vac);
-    carry = _addcarry_u64(0, ac, vac[0], &ac);
-    carry = _addcarry_u64(carry, ac, vac[1], &ac);
-    carry = _addcarry_u64(carry, ac, vac[2], &ac);
-    carry = _addcarry_u64(carry, ac, vac[3], &ac);
-    ac += carry;
-
+    ac = addc_fold_epi32(vac, ac);
     ac = csum_31bytes(b, size, ac);
 
     return ac;
@@ -134,9 +131,8 @@ uint64_t fastcsum_nofold_avx2(const uint8_t *b, size_t size, uint64_t initial) {
 
 uint64_t fastcsum_nofold_avx2_align(const uint8_t *b, size_t size, uint64_t initial) {
     unsigned long long ac = initial;
-    unsigned char carry;
-    __m256i vac{};
-    __m256i vdt, vsum, vcarry;
+    __m256i vac = _mm256_setzero_si256();
+    __m256i v, s, c;
     __m256i mask = _mm256_set1_epi32(0x80000000);
     bool needs_flip = false;
 
@@ -154,68 +150,47 @@ uint64_t fastcsum_nofold_avx2_align(const uint8_t *b, size_t size, uint64_t init
     }
 
     while (size >= 128) {
-        vdt = _mm256_load_si256(reinterpret_cast<const __m256i *>(b));
-        vsum = _mm256_add_epi32(vac, vdt);
-        vcarry = _mm256_cmpgt_epi32(_mm256_xor_si256(mask, vac), _mm256_xor_si256(mask, vsum));
-        vcarry = _mm256_srli_epi32(vcarry, 31);
-        vac = _mm256_add_epi32(vsum, vcarry);
+        v = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b));
+        addc_epi32(s, c, vac, v, mask);
+        vac = _mm256_add_epi32(s, c);
 
-        vdt = _mm256_load_si256(reinterpret_cast<const __m256i *>(b + 32));
-        vsum = _mm256_add_epi32(vac, vdt);
-        vcarry = _mm256_cmpgt_epi32(_mm256_xor_si256(mask, vac), _mm256_xor_si256(mask, vsum));
-        vcarry = _mm256_srli_epi32(vcarry, 31);
-        vac = _mm256_add_epi32(vsum, vcarry);
+        v = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b + 32));
+        addc_epi32(s, c, vac, v, mask);
+        vac = _mm256_add_epi32(s, c);
 
-        vdt = _mm256_load_si256(reinterpret_cast<const __m256i *>(b + 64));
-        vsum = _mm256_add_epi32(vac, vdt);
-        vcarry = _mm256_cmpgt_epi32(_mm256_xor_si256(mask, vac), _mm256_xor_si256(mask, vsum));
-        vcarry = _mm256_srli_epi32(vcarry, 31);
-        vac = _mm256_add_epi32(vsum, vcarry);
+        v = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b + 64));
+        addc_epi32(s, c, vac, v, mask);
+        vac = _mm256_add_epi32(s, c);
 
-        vdt = _mm256_load_si256(reinterpret_cast<const __m256i *>(b + 96));
-        vsum = _mm256_add_epi32(vac, vdt);
-        vcarry = _mm256_cmpgt_epi32(_mm256_xor_si256(mask, vac), _mm256_xor_si256(mask, vsum));
-        vcarry = _mm256_srli_epi32(vcarry, 31);
-        vac = _mm256_add_epi32(vsum, vcarry);
+        v = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b + 96));
+        addc_epi32(s, c, vac, v, mask);
+        vac = _mm256_add_epi32(s, c);
 
         b += 128;
         size -= 128;
     }
     if (size >= 64) {
-        vdt = _mm256_load_si256(reinterpret_cast<const __m256i *>(b));
-        vsum = _mm256_add_epi32(vac, vdt);
-        vcarry = _mm256_cmpgt_epi32(_mm256_xor_si256(mask, vac), _mm256_xor_si256(mask, vsum));
-        vcarry = _mm256_srli_epi32(vcarry, 31);
-        vac = _mm256_add_epi32(vsum, vcarry);
+        v = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b));
+        addc_epi32(s, c, vac, v, mask);
+        vac = _mm256_add_epi32(s, c);
 
-        vdt = _mm256_load_si256(reinterpret_cast<const __m256i *>(b + 32));
-        vsum = _mm256_add_epi32(vac, vdt);
-        vcarry = _mm256_cmpgt_epi32(_mm256_xor_si256(mask, vac), _mm256_xor_si256(mask, vsum));
-        vcarry = _mm256_srli_epi32(vcarry, 31);
-        vac = _mm256_add_epi32(vsum, vcarry);
+        v = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b + 32));
+        addc_epi32(s, c, vac, v, mask);
+        vac = _mm256_add_epi32(s, c);
 
         b += 64;
         size -= 64;
     }
     if (size >= 32) {
-        vdt = _mm256_load_si256(reinterpret_cast<const __m256i *>(b));
-        vsum = _mm256_add_epi32(vac, vdt);
-        vcarry = _mm256_cmpgt_epi32(_mm256_xor_si256(mask, vac), _mm256_xor_si256(mask, vsum));
-        vcarry = _mm256_srli_epi32(vcarry, 31);
-        vac = _mm256_add_epi32(vsum, vcarry);
+        v = _mm256_loadu_si256(reinterpret_cast<const __m256i_u *>(b));
+        addc_epi32(s, c, vac, v, mask);
+        vac = _mm256_add_epi32(s, c);
 
         b += 32;
         size -= 32;
     }
 
-    __v4di out[4];
-    _mm256_store_si256(&out[0], vac);
-    carry = _addcarry_u64(0, ac, vac[0], &ac);
-    carry = _addcarry_u64(carry, ac, vac[1], &ac);
-    carry = _addcarry_u64(carry, ac, vac[2], &ac);
-    carry = _addcarry_u64(carry, ac, vac[3], &ac);
-    ac += carry;
-
+    ac = addc_fold_epi32(vac, ac);
     ac = csum_31bytes(b, size, ac);
 
     if (needs_flip)
@@ -224,15 +199,8 @@ uint64_t fastcsum_nofold_avx2_align(const uint8_t *b, size_t size, uint64_t init
     return ac;
 }
 
-static inline void addc_epi32(__m256i &s, __m256i &c, __m256i a, __m256i b, __m256i mask) {
-    s = _mm256_add_epi32(a, b);
-    c = _mm256_cmpgt_epi32(_mm256_xor_si256(mask, a), _mm256_xor_si256(mask, s));
-    c = _mm256_srli_epi32(c, 31);
-}
-
 uint64_t fastcsum_nofold_avx2_v2(const uint8_t *b, size_t size, uint64_t initial) {
     unsigned long long ac = initial;
-    unsigned char carry;
     __m256i vac = _mm256_setzero_si256();
     __m256i v, s, c;
     __m256i mask = _mm256_set1_epi32(0x80000000);
@@ -282,14 +250,7 @@ uint64_t fastcsum_nofold_avx2_v2(const uint8_t *b, size_t size, uint64_t initial
         size -= 32;
     }
 
-    __v4di out[4];
-    _mm256_store_si256(&out[0], vac);
-    carry = _addcarry_u64(0, ac, vac[0], &ac);
-    carry = _addcarry_u64(carry, ac, vac[1], &ac);
-    carry = _addcarry_u64(carry, ac, vac[2], &ac);
-    carry = _addcarry_u64(carry, ac, vac[3], &ac);
-    ac += carry;
-
+    ac = addc_fold_epi32(vac, ac);
     ac = csum_31bytes(b, size, ac);
 
     return ac;
